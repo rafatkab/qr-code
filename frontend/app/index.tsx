ac5508e4dd40f944d92cdc7834c7b6eecce5a04b
rafatkab <rafat.kabir301@gmail.com>
2024-11-26 14:42:55 -0500

    added tailwindcss

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,15 +1,17 @@
 import { Text, View } from "react-native";
+import QRCode from "react-qr-code";
+import "../global.css";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      <Text className="text-4xl">Hello World</Text>
+      <QRCode
+        size={256}
+        style={{ height: "auto", maxWidth: "25%", width: "25%" }}
+        value={"Hello"}
+        viewBox={`0 0 256 256`}
+      />
     </View>
   );
 }
