844319df885fdf036e23acdf01332aea0891d4c4
rafatkab <rafat.kabir301@gmail.com>
2024-12-03 19:24:38 -0500

    finished

--- frontend-react/src/App.tsx
+++ frontend-react/src/App.tsx
@@ -3,44 +3,27 @@ import { QrReader } from "react-qr-reader";
 import "./App.css";
 
 function App() {
-  useEffect(() => {
-    window.addEventListener("click", () => {
-      navigator.bluetooth
-        .requestDevice({ acceptAllDevices: true })
-        .then((device) => console.log(device))
-        .catch((error) => console.log(error));
-    });
-  }, []);
-
   const [data, setData] = useState("No result");
 
-  // function onResult(result, error) {
-  //         if (!!result) {
-  //           setData(result.text);
-  //         }
-
-  //         if (!!error) {
-  //           console.info(error);
-  //         }
-  //       }
-
   return (
-    <>
-      <QrReader
-        onResult={(result, error) => {
-          if (!!result) {
-            setData(result?.getText());
-          }
+    <div>
+      <p style={{ fontSize: "2rem", margin: "0px" }}>{data}</p>
+      <center>
+        <QrReader
+          onResult={(result, error) => {
+            if (!!result) {
+              setData(result?.getText());
+            }
 
-          if (!!error) {
-            console.info(error);
-          }
-        }}
-        constraints={{ facingMode: "user" }}
-      />
-      <p>{data}</p>
-      <button>Click me</button>
-    </>
+            if (!!error) {
+              console.info(error);
+            }
+          }}
+          constraints={{ facingMode: "user" }}
+          containerStyle={{ width: "50%", border: "rounded-full" }}
+        />
+      </center>
+    </div>
   );
 }
 
